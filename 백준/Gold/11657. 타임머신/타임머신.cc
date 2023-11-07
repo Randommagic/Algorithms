d3dcf5103198134fecd0561670e922d158fdd197
BoMin Seo <68057907+Randommagic@users.noreply.github.com>
2023-11-07 16:15:15 +0900

    [Gold IV] Title: 타임머신, Time: 12 ms, Memory: 2544 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#define MAX 99999999
+using namespace std;
+
+int main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, M;
+    int st, dst, cost;
+
+    cin >> N >> M;
+
+    vector<vector<int>> edges;
+    while (M--) {
+        cin >> st >> dst >> cost;
+        edges.push_back({st, dst, cost});
+    }
+
+    vector<long long> dist(N + 1, MAX);
+    dist[1] = 0;
+
+    // N-1  + 1 번 반복하기.
+    // 마지막 한번 더 체크할때  달라지는게 있다 = 음의 사이클 발견.
+    for (int i = 1; i <= N; i++) {
+        for (const auto &edge : edges) {
+            st = edge[0];
+            dst = edge[1];
+            cost = edge[2];
+
+            // 방문처리되지 않은 노드일 경우 pass.
+            if (dist[st] == MAX)
+                continue;
+            // st -> dst 까지 거리 다시 계산.
+            if (dist[dst] > dist[st] + cost) {
+                if (i == N) {
+                    cout << -1;
+                    return 0;
+                }
+                dist[dst] = dist[st] + cost;
+            }
+        }
+    }
+
+    for (int i = 2; i <= N; i++)
+        cout << (dist[i] == MAX ? -1 : dist[i]) << "\n";
+}
